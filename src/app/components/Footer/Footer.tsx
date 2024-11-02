f834b5787ace4bb0608744ec2521483d32c2b5cf
Xenko10 <kubinios_2002@o2.pl>
2024-11-02 14:52:53 +0100

    refactor Footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,13 +2,11 @@ import styles from "./Footer.module.css";
 import InfoLinks from "./InfoLinks/InfoLinks";
 import Copyright from "./Copyright/Copyright";
 
-export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      <div className={styles.content}>
-        <InfoLinks />
-        <Copyright />
-      </div>
-    </div>
-  );
-}
+const Footer = () => (
+  <div className={styles.footer}>
+    <InfoLinks />
+    <Copyright />
+  </div>
+);
+
+export default Footer;
