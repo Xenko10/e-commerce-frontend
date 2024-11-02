4f25c4f06b1497bb1448cff2b8804b09005ea458
Xenko10 <kubinios_2002@o2.pl>
2024-11-02 14:54:28 +0100

    refactor

--- src/app/components/Footer/Copyright/Copyright.tsx
+++ src/app/components/Footer/Copyright/Copyright.tsx
@@ -1,12 +1,12 @@
 import styles from "./Copyright.module.css";
 
-export default function Copyright() {
-  return (
-    <div className={styles.copyrightWrapper}>
-      <p className={styles.copyright}>
-        © Copyright <a href='https://github.com/Xenko10'>Xenko </a>
-        {new Date().getFullYear()}. All rights reserved
-      </p>
-    </div>
-  );
-}
+const Copyright = () => (
+  <div className={styles.copyrightWrapper}>
+    <p className={styles.copyright}>
+      © Copyright <a href='https://github.com/Xenko10'>Xenko </a>
+      {new Date().getFullYear()}. All rights reserved
+    </p>
+  </div>
+);
+
+export default Copyright;
