3552b912c4858fae55e5bf7af2521d6fab6ec8cc
Jakub Kołaczyński <kubinios_2002@o2.pl>
2024-12-28 09:30:34 +0100

    get rid of timer

--- src/app/home/FlashSales/FlashSales.tsx
+++ src/app/home/FlashSales/FlashSales.tsx
@@ -1,5 +1,4 @@
 import styles from "./FlashSales.module.css";
-import Timer from "./Timer/Timer";
 import ImageSlider from "./ImageSlider/ImageSlider";
 
 const FlashSales = () => (
@@ -11,7 +10,6 @@ const FlashSales = () => (
       </div>
       <div className={styles.flashSalesTimerWrapper}>
         <h2>Flash Sales</h2>
-        <Timer />
       </div>
       <ImageSlider />
     </div>
