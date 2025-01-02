19b755239d5cf64a06f72948103ddeb53dc1969d
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-02 20:50:06 +0100

    account page

---
+++ src/app/account/Account.tsx
@@ -0,0 +1,9 @@
+import styles from "./Account.module.css";
+
+const Account = () => (
+  <div className={styles.contentWrapper}>
+    <div>Account</div>
+  </div>
+);
+
+export default Account;
---
+++ src/app/account/page.tsx
@@ -0,0 +1,5 @@
+import Account from "./Account";
+
+const page = () => <Account />;
+
+export default page;
