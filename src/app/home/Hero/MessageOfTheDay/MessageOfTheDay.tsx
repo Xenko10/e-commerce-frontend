2cc04667cf0b9e5891a0b0154f499ea2dde8288a
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-02 21:19:16 +0100

    get rid of unnecessary code

--- src/app/home/Hero/MessageOfTheDay/MessageOfTheDay.tsx
+++ src/app/home/Hero/MessageOfTheDay/MessageOfTheDay.tsx
@@ -1,10 +1,8 @@
 import styles from "./MessageOfTheDay.module.css";
 
 const MessageOfTheDay = () => (
-  <div className={styles.content}>
-    <div className={styles.message}>
-      <img src='./img/hero/iphone14.png' alt='Iphone14 up to 10% off voucher' />
-    </div>
+  <div className={styles.message}>
+    <img src="./img/hero/iphone14.png" alt="Iphone14 up to 10% off voucher" />
   </div>
 );
 
