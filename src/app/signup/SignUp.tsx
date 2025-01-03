4586cc32d50e4efaa9cbc560fa7f4c44d7eb8721
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-03 20:40:44 +0100

    add signup logic

--- src/app/signup/SignUp.tsx
+++ src/app/signup/SignUp.tsx
@@ -2,13 +2,16 @@
 
 import styles from "./SignUp.module.css";
 import { ChangeEvent, useState } from "react";
+import axios from "axios";
+import { API_URL } from "@/helpers/constant";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
-    name: "",
     email: "",
     password: "",
   });
+  const [error, setError] = useState<string>("");
+  const [success, setSuccess] = useState<string>("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -18,8 +21,31 @@ const SignUp = () => {
     }));
   };
 
-  // TODO make submit button do something
-  // TODO add validation
+  const handleSubmit = async () => {
+    try {
+      await axios.post(`${API_URL}/account/register`, {
+        email: formData.email,
+        password: formData.password,
+      });
+      setError("");
+      setSuccess("Account created successfully");
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 4000);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const errorMessage = error.response?.data
+          .map((error: string) => error)
+          .join("<br />");
+        setError(errorMessage);
+      } else {
+        setError("An error occurred");
+      }
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
 
   return (
     <div className={styles.contentWrapper}>
@@ -28,14 +54,6 @@ const SignUp = () => {
         <p className={styles.paragraph}>Enter you details bellow</p>
       </div>
       <div className={styles.formWrapper}>
-        <input
-          type="text"
-          name="name"
-          className={styles.input}
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleChange}
-        />
         <input
           type="text"
           name="email"
@@ -52,7 +70,21 @@ const SignUp = () => {
           value={formData.password}
           onChange={handleChange}
         />
-        <button className={styles.createAccountBtn}>Create account</button>
+        <button
+          onClick={() => {
+            handleSubmit();
+          }}
+          className={styles.createAccountBtn}
+        >
+          Create account
+        </button>
+        {error !== "" && (
+          <div
+            className={styles.error}
+            dangerouslySetInnerHTML={{ __html: error }}
+          />
+        )}
+        {success !== "" && <div className={styles.success}>{success}</div>}
       </div>
       <div className={styles.alreadyAccountWrapper}>
         <span className={styles.alreadyAccount}>Already have an account?</span>
