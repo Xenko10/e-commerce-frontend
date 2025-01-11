c87ea680693fe5776b8862581983c2fb8fd2b509
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-11 14:30:28 +0100

    add logout

--- src/app/profile/Profile.tsx
+++ src/app/profile/Profile.tsx
@@ -1,9 +1,33 @@
-import styles from "./Account.module.css";
+"use client";
 
-const Profile = () => (
-  <div className={styles.contentWrapper}>
-    <div>Profile</div>
-  </div>
-);
+import styles from "./Account.module.css";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+
+const Profile = () => {
+  const [cookies] = useCookies(["Exclusive.UserId"]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setIsLoggedIn(!!cookies["Exclusive.UserId"]);
+  }, [cookies]);
+
+  return (
+    <div className={styles.contentWrapper}>
+      <h1>Profile</h1>
+      {isLoggedIn && (
+        <button
+          onClick={() => {
+            document.cookie =
+              "Exclusive.UserId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            setIsLoggedIn(false);
+          }}
+        >
+          Log out
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Profile;
