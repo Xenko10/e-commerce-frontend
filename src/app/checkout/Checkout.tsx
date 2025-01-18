8f2647a1a92a09fe302d4325066dce71505355d6
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-18 13:12:15 +0100

    add media query in checkout

--- src/app/checkout/Checkout.tsx
+++ src/app/checkout/Checkout.tsx
@@ -1,16 +1,15 @@
 import styles from "./Checkout.module.css";
+
 import BillingDetails from "./components/BillingDetails/BillingDetails";
 import Summary from "./components/Summary/Summary";
 
-const Checkout = () => {
-  return (
-    <div>
-      <div className={styles.contentWrapper}>
-        <BillingDetails />
-        <Summary />
-      </div>
+const Checkout = () => (
+  <div>
+    <div className={styles.contentWrapper}>
+      <BillingDetails />
+      <Summary />
     </div>
-  );
-};
+  </div>
+);
 
 export default Checkout;
