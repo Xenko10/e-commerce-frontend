91f02eea00a6e4f67b5c43a6c379c81693cb2ab7
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-18 14:58:59 +0100

    add css

--- src/app/order-placed/OrderPlaced.tsx
+++ src/app/order-placed/OrderPlaced.tsx
@@ -1,9 +1,13 @@
+import styles from "./OrderPlaced.module.css";
+
 // TODO show order number and give info that
 // order will be realized when payment to certain account is made
 const OrderPlaced = () => {
   return (
     <div>
-      <h1>Order Placed</h1>
+      <div className={styles.contentWrapper}>
+        <h1>Order Placed</h1>
+      </div>
     </div>
   );
 };
