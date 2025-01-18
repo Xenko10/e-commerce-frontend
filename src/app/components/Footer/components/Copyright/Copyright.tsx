4cb512f7b27bd11157eff969279a66ed03b0c1e9
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-18 08:40:18 +0100

    change copyright

--- src/app/components/Footer/components/Copyright/Copyright.tsx
+++ src/app/components/Footer/components/Copyright/Copyright.tsx
@@ -3,7 +3,7 @@ import styles from "./Copyright.module.css";
 const Copyright = () => (
   <div className={styles.copyrightWrapper}>
     <p className={styles.copyright}>
-      © Copyright <a href='https://github.com/Xenko10'>Xenko </a>
+      © Copyright <a href="https://github.com/Xenko10">Jakub Kołaczyński </a>
       {new Date().getFullYear()}. All rights reserved
     </p>
   </div>
