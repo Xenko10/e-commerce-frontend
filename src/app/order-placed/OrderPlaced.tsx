c4714054b68ba0dabe5c0a73a8ff47261ac8684e
Jakub Kołaczyński <kubinios_2002@o2.pl>
2025-01-19 07:20:37 +0100

    add info

--- src/app/order-placed/OrderPlaced.tsx
+++ src/app/order-placed/OrderPlaced.tsx
@@ -1,15 +1,14 @@
 import styles from "./OrderPlaced.module.css";
 
-// TODO show order number and give info that
-// order will be realized when payment to certain account is made
-const OrderPlaced = () => {
-  return (
-    <div>
-      <div className={styles.contentWrapper}>
-        <h1>Order Placed</h1>
-      </div>
+// TODO show order number
+const OrderPlaced = () => (
+  <div>
+    <div className={styles.contentWrapper}>
+      <h1>Order Placed</h1>
+      <p>Your order will be realized when payment to account bellow is made.</p>
+      <p>Account number: PL25 1090 2402 7488 8851 8982 1385</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default OrderPlaced;
